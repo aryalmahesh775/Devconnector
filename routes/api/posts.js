1c935ff69e3dae5062989a8ac92878f917edf4fa
aryalmahesh775 <aryalmahesh775@gmail.com>
2020-05-06 15:50:00 +0545

    Express and mongo setup

---
+++ routes/api/posts.js
@@ -0,0 +1,12 @@
+// form area where addd post like dislike
+const express = require('express')
+const router = express.Router()
+
+// @route  GET api/posts
+//@desc    Test route
+//@access  Public
+router.get('/', (req, res) => {
+  res.send('Post route')
+})
+
+module.exports = router
---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+//register user  adding user
+
+const express = require('express')
+const router = express.Router()
+
+// @route  GET api/users
+//@desc    Test route
+//@access  Public
+router.get('/', (req, res) => {
+  res.send('User route')
+})
+
+module.exports = router
