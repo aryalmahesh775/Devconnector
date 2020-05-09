7769c36b733405492604ac9371c604144d4dfea0
aryalmahesh775 <aryalmahesh775@gmail.com>
2020-05-09 08:27:58 +0545

    Get all profile and profile by user id

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose')
-const config = require('config')
-const db = config.get('mongoURI')
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
@@ -8,14 +8,15 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-    })
+      useFindAndModify: false,
+    });
 
-    console.log('MongoDB Connected....')
+    console.log('MongoDB Connected....');
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
     // Exit process with failure
-    process.exit(1)
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
