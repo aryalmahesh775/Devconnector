5bcab357cf8fe86ab42284bf6dfc3d701e218617
aryalmahesh775 <aryalmahesh775@gmail.com>
2020-05-30 12:34:55 +0545

    Adding comment, sisplay and delete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/post';
+import CommentForm from './CommentForm';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -19,6 +21,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back to Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
